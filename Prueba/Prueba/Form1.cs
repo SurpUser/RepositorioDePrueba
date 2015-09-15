3c052e82d267302ba84cfec7cb51be8ffa72bc17
Francis Castillo <franciselcaliente@gmail.com>
2015-09-15 10:57:06 +0100

    nuevo evento

--- Prueba/Prueba/Form1.cs
+++ Prueba/Prueba/Form1.cs
@@ -16,5 +16,10 @@ namespace Prueba
         {
             InitializeComponent();//inicializa los componentes
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("hola");
+        }
     }
 }
